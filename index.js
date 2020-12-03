5af29956b465d0e3386647a23a50b799f9771842
KevinMoreno <kmoreno@labelti.com>
2020-12-02 22:26:55 -0600

    added movieRoutes doc to swagger

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const swaggerOptions = {
             "servers": ["http://localhost:" + port]
         }
     },
-    apis: ['index.js', './routes/index.js']
+    apis: ['index.js', './routes/index.js', './routes/movieRoutes.js']
 }
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
