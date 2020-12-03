cdd438f6f3151569783c52770a6f17146060c2f6
KevinMoreno <kmoreno@labelti.com>
2020-12-02 23:08:28 -0600

    added swagger to watched routes

--- routes/watchedRoutes.js
+++ routes/watchedRoutes.js
@@ -7,34 +7,34 @@ const watchedController = require('../src/controllers/watched.controller');
 
 /**
  * @swagger
- * /:
+ * /watched/get:
  *  get:
- *      description: get Watched List from Database
+ *      description: get Watched movies of a user according to the current token
  *      responses: 
  *          200:
- *              description: get Data from DataBase and render with handlebars
+ *              description:an array with all the watched movies by the current user
  */
 router.get('/get', watchedController.getWatched);
 
 /**
  * @swagger
- * /:
+ * /watched/post:
  *  post:
- *      description: get Watched List from Database
+ *      description: Post in DB a watched movie by the user with the current token
  *      responses: 
  *          200:
- *              description: get Data from DataBase and render with handlebars
+ *              description:success
  */
 router.post('/post', watchedController.postWatched);
 
 /**
  * @swagger
- * /:
+ * /watched/delete:
  *  delete:
- *      description: delete Watched watched from Database
+ *      description: Delete a movie from the Watched list of the user with the current token
  *      responses: 
  *          200:
- *              description: delete 
+ *              description:movie deleted
  */
 router.delete('/delete', watchedController.deleteWatched);
 
